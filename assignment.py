183c525ee0f2a9934f001fa4ea4e54231c74ca36
KDaniel <khderdaniel.77@gmail.com>
2025-02-13 16:41:38 +0200

    wtd

--- assignment.py
+++ assignment.py
@@ -1,19 +1,25 @@
 #first project
-
-#a = int(input("Hi user, Enter your age "))
-#if a <= 5 :
-# print(" the ticket is for free ")
-#elif a <= 12 :
-# print (" The ticket price is $5 ")
-#elif a <= 18:
-# print (" The ticket price is $7 ")
-#else:
-# print (" The ticket price is $10 ")
+a = int(input("Hi user, Enter your age "))
+if a <= 5 :
+ print(" the ticket is for free ")
+elif a <= 12 :
+ print (" The ticket price is $5 ")
+elif a <= 18:
+ print (" The ticket price is $7 ")
+else:
+ print (" The ticket price is $10 ")
 
  #second project
-
 a = float(input("Hi user, Enter a number "))
 if a % 2 == 0 :
     print("the number you enter it is even ")
 else:
     print ("the number you enter it is odd ")
+
+#third project
+a = input ('please enter your name ')
+b = input (' please enter your password ')
+if a == "admin" and b == "1234" :
+    print ('Access granted')
+else:
+    print ("Access denied")
