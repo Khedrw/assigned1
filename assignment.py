30538a5de2e9c8dbb5dcc06b3d56cc3e1f379f20
KDaniel <khderdaniel.77@gmail.com>
2025-02-13 16:34:12 +0200

    finish exercise 2

--- assignment.py
+++ assignment.py
@@ -1,9 +1,19 @@
-a = int(input("Hi user, Enter your age "))
-if a <= 5 :
- print(" the ticket is for free ")
-elif a <= 12 :
- print (" The ticket price is $5 ")
-elif a <= 18:
- print (" The ticket price is $7 ")
+#first project
+
+#a = int(input("Hi user, Enter your age "))
+#if a <= 5 :
+# print(" the ticket is for free ")
+#elif a <= 12 :
+# print (" The ticket price is $5 ")
+#elif a <= 18:
+# print (" The ticket price is $7 ")
+#else:
+# print (" The ticket price is $10 ")
+
+ #second project
+
+a = float(input("Hi user, Enter a number "))
+if a % 2 == 0 :
+    print("the number you enter it is even ")
 else:
- print (" The ticket price is $10 ")
+    print ("the number you enter it is odd ")
