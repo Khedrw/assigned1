6fc1fd4a3abb4618dbb9ea27841d3d738acfe589
KDaniel <khderdaniel.77@gmail.com>
2025-02-13 16:14:50 +0200

    finish exercise 1

---
+++ assignment.py
@@ -0,0 +1,9 @@
+a = int(input("Hi user, Enter your age "))
+if a <= 5 :
+ print(" the ticket is for free ")
+elif a <= 12 :
+ print (" The ticket price is $5 ")
+elif a <= 18:
+ print (" The ticket price is $7 ")
+else:
+ print (" The ticket price is $10 ")
